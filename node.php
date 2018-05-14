72a8b48736a3f5e6d186112d5b72fe5fbd727496
Yanbin Zhu <haker-haker@163.com>
2018-05-14 18:07:55 +0800

    节点图

--- node.php
+++ node.php
@@ -39,23 +39,139 @@ function getAllRule(PDO $pdo) {
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function buildNodeTree($dataArray) {
+/**
+ * @param array $dataArray
+ * @return array
+ */
+function buildNodeParentIndex(array $dataArray) {
     $result = [];
     foreach ($dataArray as $data) {
-        $parent = intval($data['parent_id']);
-        if (!isset($result[$parent])) {
-            $result[$parent] = [];
-        }
-        $result[$parent][] = $data;
+        $parentId = intval($data['parent_id']);
+        $nodeId = intval($data['node_id']);
+        $result[$nodeId] = $parentId;
     }
+
+    return $result;
+}
+
+/**
+ * @param array $indexArray
+ * @return array
+ */
+function buildNodeTreeIndex(array $indexArray) {
+    $result = [];
+
+    foreach ($indexArray as $k => $v) {
+        if ($k > 0) {
+            $result[$k] = findParent($indexArray, $k, []);
+        }
+    }
+
+    return $result;
+}
+
+/**
+ * @param array $dataArray
+ * @param $nodeId
+ * @param array $path
+ * @return array
+ */
+function findParent(array $dataArray, $nodeId, array $path) {
+
+    if (isset($dataArray[$nodeId])) {
+        $parentId = $dataArray[$nodeId];
+        if ($parentId != 0) {
+            $path[] = $parentId;
+            return findParent($dataArray, $parentId, $path);
+        }
+    }
+
+    $path[] = 0;
+    return $path;
+}
+
+function formatNodeTree(array &$dataArray) {
+    if (sizeof($dataArray) > 1 && isset($dataArray[0])) {
+        unset($dataArray[0]);
+    }
+
+    foreach ($dataArray as $k => $v) {
+        if (is_array($v)) {
+            $dataArray[$k] = formatNodeTree($v);
+        }
+    }
+
+    if ((sizeof($dataArray) == 1) && (isset($dataArray[0]))) {
+        $dataArray = $dataArray[0];
+    }
+
+    return $dataArray;
+}
+
+function buildNodeNameMap(array $dataArray) {
+    $result = [0 => ['name' => '风控根节点', 'id' => 0, 'weight' => 0]];
+    foreach ($dataArray as $v) {
+        $id = intval($v['id']);
+        $result[$id] = ["id" => $id, 'name' => $v['name'], 'weight' => $v['weight']];
+    }
+
     return $result;
 }
 
 $nodeArray = getAllNodeData($pdo);
+$nodeNameArray = buildNodeNameMap($nodeArray);
+
 $nodeRelationArray = getAllNodeRelations($pdo);
 $nodeMapArray = getAllNodeMap($pdo);
 $ruleArray = getAllRule($pdo);
 
-$nodeTree = buildNodeTree($nodeRelationArray);
+$nodeParentIndexArray = buildNodeParentIndex($nodeRelationArray);
+$nodeIndexArray = buildNodeTreeIndex($nodeParentIndexArray);
 
-var_dump($nodeTree);
+$dataArray = [];
+$result = array_walk($nodeIndexArray,
+    function ($item, $key) use (&$dataArray){
+        $data = &$dataArray;
+        foreach (array_reverse($item) as $v) {
+            if (!isset($data[$v])) {
+                $data[$v] = [$key];
+            }
+
+            $data = &$data[$v];
+        }
+    });
+if (!$result) {
+    die("build node fail.\n");
+}
+
+
+$dataArray = formatNodeTree($dataArray);
+
+function output($data, array $nodeNameArray) {
+    $result = "";
+    if (is_array($data)) {
+        foreach ($data as $k => $v) {
+            $result .= "{";
+            $result .= "\"name\": \"{$nodeNameArray[$k]['name']}\",";
+            $result .= "\"children\": [";
+            $result .= substr(output($v, $nodeNameArray), 0, -1);
+            $result .= "]";
+            $result .= "},";
+        }
+    } else {
+        $result .= "{\"name\": \"{$nodeNameArray[$data]['name']}\"},";
+    }
+
+    return $result;
+}
+
+$dataArray = [0 => $dataArray];
+unset($dataArray[0][1]);
+$result = output($dataArray, $nodeNameArray);
+$result = substr($result, 0 , -1);
+
+$fp = fopen(__DIR__ . '/view/js/node_data.json', 'wb+');
+fwrite($fp, $result);
+fclose($fp);
+
+echo "导出成功\n";
